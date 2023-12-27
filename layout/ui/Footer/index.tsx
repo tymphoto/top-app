626c406650181cc53ffdeb1963bb28d787c19c5a
Artem Timoshenko <atimoshenko@ubic.tech>
2023-12-27 17:47:55 +0300

    add grid templates to layout, add date-fns

--- layout/ui/Footer/index.tsx
+++ layout/ui/Footer/index.tsx
@@ -1,17 +1,37 @@
 import { DetailedHTMLProps, HTMLAttributes } from 'react';
+import cn from 'classnames';
+import { format } from 'date-fns';
 import styles from './styles.module.scss';
 
-interface FooterProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> { }
+interface FooterProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  className?: string;
+}
 
 export const Footer = ({
+  className,
   ...props
 }: FooterProps): JSX.Element => {
   return (
-    <div
-      className={styles.Footer}
+    <footer
+      className={cn(className, styles.footer)}
       {...props}
     >
-      Footer
-    </div>
+      <div>
+        TopApp {format(new Date(), 'yyyy')} Все права защищены
+      </div>
+      <a
+        href='#'
+        target='_blank'
+      >
+        Пользовательское соглашение
+      </a>
+      <a
+        href='#'
+        target='_blank'
+      >
+        Политика конфиденциальности
+      </a>
+
+    </footer>
   );
 };
--- layout/ui/Layout/Layout.tsx
+++ layout/ui/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 import { Header } from '../Header';
 import { Sidebar } from '../Sidebar';
 import { Footer } from '../Footer';
+import styles from './styles.module.scss';
 
 interface LayoutProps {
   children: ReactNode;
@@ -11,15 +12,13 @@ export const Layout = ({
   children,
 }: LayoutProps): JSX.Element => {
   return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        <div>
-          {children}
-        </div>
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body} >
+        {children}
       </div>
-      <Footer />
-    </>
+      <Footer className={styles.footer} />
+    </div>
   );
 };
